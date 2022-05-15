9a7848d016c9cae30c0d1057a05437a74642034d
captain-yossarian <sergiybiluk@gmail.com>
2022-05-15 20:09:24 +0200

    small fix

--- 4_array_includes_solution.ts
+++ 4_array_includes_solution.ts
@@ -1,26 +1,25 @@
+const GREAT_COUNTRIES = ['Poland', 'USA', 'Lithuania', 'Latvia', 'Estonia', 'Ukraine'] as const;
+// 2 -> number
+type BackwardInference<T, P> =
+    P extends any ? T extends P ? P : never : never;
 
-const PROPS = ['a', 'b', 'c'] as const;
-
-type InferPrimitive<T, P> = P extends any ? T extends P ? P : never : never;
-
-type Inference<T> = InferPrimitive<T, Primitives>
-
+type Generalize<T> = BackwardInference<T, Primitives>
 {
-    type _ = Inference<2 | 's'> // stirng | number
-    type __ = Inference<42> //  number
+    type _ = Generalize<2 | 's'> // stirng | number
+    type __ = Generalize<42> //  number
 }
 
 const withTuple = <
     List extends Primitives[]
 >(list: readonly [...List]) =>
-    (prop: Inference<List[number]>):
-        prop is Inference<List[number]> & List[number] =>
+    (prop: Generalize<List[number]>):
+        prop is Generalize<List[number]> & List[number] =>
         list.includes(prop)
 
-const includes = withTuple(PROPS);
+const includes = withTuple(GREAT_COUNTRIES);
 
 includes(2)       // expected error
-includes(['str']) // expected error
+includes(['russia']) // expected error
 
 const result = includes('d') // ok
 
